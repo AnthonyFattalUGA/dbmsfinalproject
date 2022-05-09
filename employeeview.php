331cfc8737ba4755d0036e8b129d2a1c7897cefa
Dev Patel <63990626+devdp2001@users.noreply.github.com>
2022-05-09 16:39:33 -0400

    Update employeeview.php
    changed style to dtstyle.css

--- employeeview.php
+++ employeeview.php
@@ -40,7 +40,7 @@ if (!empty($row1)) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employee Manager</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="dtstyle.css">
 </head>
 
 <body>
