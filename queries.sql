e9ec3fad80ebf2aecf5894e4393253409a16100a
AnthonyFattalUGA <82898602+AnthonyFattalUGA@users.noreply.github.com>
2022-05-09 20:57:06 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,64 +1,44 @@
--- inserts
-INSERT INTO `customers` (`cname`,`zip`,`phone_number`,`email`) VALUES ('$cname','$zip','$phone_number','$email');
-
-INSERT INTO `employees` (`ename`,`phone_number`,`email`)
-  VALUES ('$ename','$phone_number','$email');
-
-INSERT INTO `suppliers` (`sname`) 
-  VALUES ('$sname');
-
-INSERT INTO `products` (`pname`,`stock`,`price`,`category`,`sid`,`color`,`psize`) 
-  VALUES ('$pname','$stock','$price','$category','$sid','$color','$psize');
-
-INSERT INTO `orders` (`oid`, `odate`, `pid`, `cid`, `eid`, `count`) 
-  VALUES (NULL, '$currentdate',
-    '$currentpid', '$currentcid',
-    '$currenteid', '$currentamount');
-
--- selects
+-- single customer from cid
 SELECT * FROM `customers` 
   where cid=".$_REQUEST["cid"].";
- 
+
+-- all customers
 SELECT * FROM `customers`;
- 
-SELECT * FROM `customers` 
-  WHERE `cid` = '$editcid';
- 
+
+-- single employee
 SELECT * FROM `employees` 
   where `eid` = '$editeid'
 
+-- all categories
 SELECT category FROM `products` 
   GROUP BY category;
- 
+
+-- product info for one product
 SELECT *, suppliers.sname FROM `products` 
   inner join suppliers 
     where `pid` = '$editpid' 
       and products.sid=suppliers.sid;
 
-SELECT * FROM `employees` 
-  where eid=".$_REQUEST["eid"].";
-
+-- all employee info
 SELECT * FROM `employees`;
 
+-- all order info as well as names and prices
 select orders.*, customers.cname, employees.ename, products.pname, products.price 
   from orders
     left outer join customers on orders.cid = customers.cid
     left outer JOIN employees on orders.eid = employees.eid
     left outer JOIN products on products.pid = orders.pid;
-    
-SELECT *, suppliers.sname FROM `products` 
-  inner join suppliers 
-  where `category` = '$cat_id' 
-    and `sname` = '$supplier_id' 
-    and products.sid=suppliers.sid;
 
+-- products with specific category and supplier
 SELECT *, suppliers.sname FROM `products` 
   inner join suppliers 
   where `category` = '$cat_id' 
     and products.sid=suppliers.sid;
 
+-- category list
 SELECT category FROM `products` GROUP BY category;
 
+-- supplier name list
 SELECT suppliers.sname FROM `products` 
   inner join suppliers 
   where products.sid=suppliers.sid 
