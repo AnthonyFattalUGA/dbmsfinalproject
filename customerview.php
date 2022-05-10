f2492456e3efb3315308e45660bf7e24f4b05e99
Dev Patel <63990626+devdp2001@users.noreply.github.com>
2022-05-10 11:56:40 -0400

    Update customerview.php
    added link to homepage

--- customerview.php
+++ customerview.php
@@ -58,6 +58,7 @@ if (!empty($row1)) {
         <div class="flex">
             <aside class="left">
                 <h3 class="title"><a href="customerview.php">Customer Home</a></h3>
+                <h3 class="title"><a href="index.php">Home Page</a></h3>
                 <ul class="nav">
                     <?php //echo $show1; ?>
                 </ul>
